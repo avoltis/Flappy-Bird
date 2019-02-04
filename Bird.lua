8187b0c40a7b927a128668c6c07c6fc7ca781dba
Voltis <v.agolli@live.com>
2019-02-04 17:05:02 +0100

    top reverted pipes

--- Bird.lua
+++ Bird.lua
@@ -1,9 +1,9 @@
-Bird = Class{}
+Bird = Class {}
 
 local GRAVITY = 20
 
 function Bird:init()
-    self.image = love.graphics.newImage('bird.png');
+    self.image = love.graphics.newImage("bird.png")
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
 
@@ -16,13 +16,13 @@ end
 function Bird:update(dt)
     self.dy = self.dy + GRAVITY * dt
 
-    if love.keyboard.wasPressed('space') then 
+    if love.keyboard.wasPressed("space") then
         self.dy = -5
     end
 
     self.y = self.y + self.dy
-end 
+end
 
-function Bird: render()
+function Bird:render()
     love.graphics.draw(self.image, self.x, self.y)
-end 
+end
--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ require "Bird"
 
 require "Pipe"
 
+require "PipePair"
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -23,16 +25,20 @@ local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
 
+local GROUND_LOOPING_POINT = 514
+
 local bird = Bird()
 
-local pipes = {}
+local pipePairs = {}
 
 local spawnTimer = 0
+
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 
-    love.window.setTitle("Flappy Voltis")
+    love.window.setTitle("Fifty Voltis")
 
     push:setupScreen(
         VIRTUAL_WIDTH,
@@ -46,7 +52,7 @@ function love.load()
         }
     )
 
-    love.keyboard.keypressed = {}
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -54,7 +60,7 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
-    love.keyboard.keypressed[key] = true
+    love.keyboard.keysPressed[key] = true
 
     if key == "escape" then
         love.event.quit()
@@ -67,26 +73,32 @@ end
 
 function love.update(dt)
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
 
     spawnTimer = spawnTimer + dt
 
     if spawnTimer > 2 then
-        table.insert(pipes, Pipe())
+        local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+        lastY = y
+
+        table.insert(pipePairs, PipePair(y))
         spawnTimer = 0
     end
 
     bird:update(dt)
 
-    for k, pipe in pairs(pipes) do
-        pipe:update(dt)
+    for k, pair in pairs(pipePairs) do
+        pair:update(dt)
+    end
 
-        if pipe.x < -pipe.width then
-            table.remove(pipes, k)
+    for k, pair in pairs(pipePairs) do
+        if pair.remove then
+            table.remove(pipePairs, k)
         end
     end
 
-    love.keyboard.keypressed = {}
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
@@ -94,8 +106,8 @@ function love.draw()
 
     love.graphics.draw(background, -backgroundScroll, 0)
 
-    for k, pipe in pairs(pipes) do
-        pipe:render()
+    for k, pair in pairs(pipePairs) do
+        pair:render()
     end
 
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
